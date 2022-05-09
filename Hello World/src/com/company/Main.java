43122bc84e1284ee3c4aca70f406e6d4b87804c2
m4lone <caiquesilva1201@outlook.com>
2022-05-08 23:20:16 -0300

    Realizada resolução dos requisitos do desafio 1.

---
+++ Hello World/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    System.out.println("Olá Mundo");
+
+        String nome = "Caique Silva";
+        int idade = 22;
+
+        System.out.println("Meu nome é: " + nome + " e tenho: " + idade + " Anos");
+    }
+}
